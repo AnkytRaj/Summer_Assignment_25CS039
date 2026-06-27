a5e78fdb8b799772dd6fc27fad81044bc2924a1d
Ankit Raj <guts.eww@gmail.com>
2026-06-27 09:17:34 +0530

    uploaded files

---
+++ day 20/Q1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], result[3][3];
+
+    printf("Enter elements of first 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Initialize result matrix
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    // Multiply matrices
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("Product of matrices:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
